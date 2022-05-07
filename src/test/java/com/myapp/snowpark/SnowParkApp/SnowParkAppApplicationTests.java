61f471cc90ac6d0acb86144d12854598e0a93ad0
S-Rajeshkumar <s.rajeshkumar99@gmail.com>
2022-05-07 14:29:49 +0530

    Project Base Commit

---
+++ src/test/java/com/myapp/snowpark/SnowParkApp/SnowParkAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.snowpark.SnowParkApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowParkAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
